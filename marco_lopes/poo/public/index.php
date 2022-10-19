034721a7b1bb89056c628e187b35fefcdad51609
marcoAL2000 <marcoantoniolopes2016@gmail.com>
2022-10-18 22:16:01 -0300

    poo

---
+++ marco_lopes/poo/public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Unialfa\Poo\FormaBolo;
+
+$turmaBolo = new FormaBolo();
+$outroBolo = new FormaBolo();
+
+var_dump($turmaBolo);
+echo '<br>';
+var_dump($outroBolo);
