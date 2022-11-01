fdf1bdbeb5704c4958dd077df5b77d47380e0dd0
daniel claus <danclaus2002@gmail.com>
2022-11-01 19:47:55 -0300

    add project

---
+++ daniel_claus/poo/public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+use Unialfa\Poo\FormaBolo;
+
+$turmaBolo = new FormaBolo();
+$outroBolo = new FormaBolo();
+
+var_dump($turmaBolo);
+echo '<br>';
+var_dump($outroBolo);
