48fa86c75c3bf2e4129d8dc927692014350d66e0
Rhuan-Zucarelli <rhuan1zucarelli@gamil.com>
2022-11-01 22:03:41 -0300

    01/11/2022

---
+++ Rhuan-Zucarelli/poo/public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Unialfa\Poo\FormaBolo;
+
+//var_dump($_REQUEST["tempoDeForno"]);exit;
+
+$tempoDeForno = $_REQUEST["tempoDeForno"];
+
+$turmaBolo = new FormaBolo();
+$turmaBolo->fazerBolo();
+$turmaBolo->colocarNoForno($tempoDeForno);
+
