2977e7875b4e1385c9f53bbb667491fb28c12db6
Jorge Aguiar <jorgeaugusto_99@hotmail.com>
2022-11-01 21:51:23 -0300

    Adicionado Front-End ao projeto e criado metodo colocarNoForno

--- jorge_aguiar/poo/public/index.php
+++ jorge_aguiar/poo/public/index.php
@@ -1,11 +1,18 @@
-<?php 
+<?php
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use Unialfa\Poo\FormaBolo;
 
+// var_dump($_REQUEST["tempoDeForno"]); exit;
+
+$tempoDeForno = $_REQUEST["tempoDeForno"];
+
 $turmaBolo = new FormaBolo();
 $outroBolo = new FormaBolo();
+
+$outroBolo->fazerBolo();
+$outroBolo->colocarNoForno($tempoDeForno);
 
 var_dump($turmaBolo);
 echo '<br>';
