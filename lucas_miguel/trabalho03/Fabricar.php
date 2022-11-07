d3f429ec57ef839ac5e471cfbcfb810c97dbb022
luchaox <lucasmiguel7@hotmail.com>
2022-11-06 21:16:30 -0300

    Adicionando e criando o trabalho03

--- lucas_miguel/trabalho03/Fabricar.php
+++ lucas_miguel/trabalho03/Fabricar.php
@@ -1,10 +1,46 @@
 <?php 
 
-//class Fabricar {
+class Fabricar {
 
-//public function Fabrica($fabrica) {
-    //$carro = new Veiculo();
-//$carro->setModelo("Corsa");
-   
-    
-//}
+    public function Fabrica($fabrica) {
+        $carro = new Veiculo();
+        $carro2 = new Veiculo();
+        $carro3 = new Veiculo();
+        $carro4 = new Veiculo();
+
+        $cliente = new Cliente();
+        $cliente2 = new Cliente();
+        $cliente3 = new Cliente();
+        $cliente4 = new Cliente();
+
+
+    $carro->SetMarca("Mazda");
+    $carro->SetModelo("RX-7");
+    $carro->SetAno("2002");
+    $carro->SetCor("Prata");
+    $cliente->SetNome("Pedro");
+
+    $carro2->SetMarca("Nissan");
+    $carro2->SetModelo("Skyline GT-R34");
+    $carro2->SetAno("1998");
+    $carro2->SetCor("Azul");
+    $cliente2->SetNome("André");
+
+    $carro3->SetMarca("Dodge");
+    $carro3->setModelo("Viper SRT");
+    $carro3->SetAno("2013");
+    $carro3->SetCor("Vermelho");
+    $cliente3->SetNome("Thiago");
+
+    $carro4->SetMarca("Honda");
+    $carro4->SetModelo("NSX");
+    $carro4->SetAno("1995");
+    $carro4->SetCor("Branco");  
+    $cliente4->SetNome("João");
+
+
+    }
+}
+
+
+?>
--- lucas_miguel/trabalho03/Veiculo.php
+++ lucas_miguel/trabalho03/Veiculo.php
@@ -2,13 +2,42 @@
 
 class Veiculo {
 
+    public $marca;
     public $modelo;
     public $ano;
     public $cor;
 
 
+    public function getMarca($marca){
+        return $marca;
+    }
+    
+    public function setMarca($marca){
+        $this->marca = $marca;
+    }
+
+    public function getModelo($modelo){
+        return $modelo;
+    }
+
     public function setModelo($modelo) {
         $this->modelo = $modelo;
+    }
+
+    public function getAno($ano) {
+        return $ano;
+    }
+
+    public function setAno($ano) {
+        $this->ano = $ano;
+    }
+
+    public function getCor($cor) {
+        return $cor;
+    }
+
+    public function setCor($cor) {
+        $this->cor = $cor;
     }
 
 
