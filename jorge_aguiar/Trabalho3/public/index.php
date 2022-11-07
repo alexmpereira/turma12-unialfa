0124f945aae278559cf7fec1230595c0f14e2e06
Jorge Aguiar <jorgeaugusto_99@hotmail.com>
2022-11-07 10:05:55 -0300

    Comentado trecho de código para DEBUG

--- jorge_aguiar/Trabalho3/public/index.php
+++ jorge_aguiar/Trabalho3/public/index.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_erros', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_erros', 1);
+// error_reporting(E_ALL);
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use Unialfa\Poo\Marca;
