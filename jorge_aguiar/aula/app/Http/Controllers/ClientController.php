493d01d6b7e385596ea0a472fa2d2ebb9f153e68
Jorge Aguiar <jorgeaugusto_99@hotmail.com>
2023-02-24 22:18:28 -0300

    Inicio do projeto e criação de tabela clients

---
+++ jorge_aguiar/aula/app/Http/Controllers/ClientController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    public function index()
+    {
+        $clients = Client::get();
+
+        return view('clients.index', [
+            'clients' => $clients
+        ]);
+    }
+}
