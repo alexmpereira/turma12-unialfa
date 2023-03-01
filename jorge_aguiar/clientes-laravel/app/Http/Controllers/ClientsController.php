f07d985c68ce6eda66637c1997f4463cdd724ae6
Jorge Aguiar <jorgeaugusto_99@hotmail.com>
2023-02-28 22:16:24 -0300

    Criação de novo projeto

---
+++ jorge_aguiar/clientes-laravel/app/Http/Controllers/ClientsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClientsController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+}
