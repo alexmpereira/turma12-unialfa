459c7d36018b90b1f4ef8515a101c5edfa3d80f8
gabrielsantos8 <gabrieldossantossilva2108@gmail.com>
2023-03-21 21:11:13 -0300

    resource no products

--- gabriel_dos_santos_silva/cliente-laravel/routes/web.php
+++ gabriel_dos_santos_silva/cliente-laravel/routes/web.php
@@ -27,13 +27,14 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
-Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
+// Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
+// Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+// Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+// Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+// Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
 
 
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+// Route::post('/products', [ProductController::class, 'store'])->name('products.store');
 
 Route::resource('/clients', ClientController::class);
+Route::resource('/products', ProductController::class);
